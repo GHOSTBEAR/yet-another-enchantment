77043bd8ccab4fe9a2364e7073bff0d0ea0f0553
Andreas E <andreas.everos@gmail.com>
2020-03-15 13:41:09 +0100

    Setup project

---
+++ src/main/java/me/ghostbear/yetanotherenchantment/YetAnotherEnchantmentMod.java
@@ -0,0 +1,11 @@
+package me.ghostbear.yetanotherenchantment;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Reference.MOD_ID)
+public class YetAnotherEnchantmentMod {
+
+    public YetAnotherEnchantmentMod() {
+
+    }
+}
